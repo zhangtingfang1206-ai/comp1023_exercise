ab31a79b2e40514a13cf7e6ed78a26ac9d18719b
你的名字 <>
2026-05-12 11:40:35 +0800

    update

--- __oop.py
+++ __oop.py
@@ -4,16 +4,17 @@ class Student:
     def __init__(self, desmond):
         self.__name = 'g'  # two _ _ only allow internal methods can access
                            # new name: _Student__name
-        self._age = 4
+        self._age = 2
         self.instructor = desmond
-
     def print(self):
         print(self.__name)
     def get_score(self):
-        score = np.array(([2,3,4],
+        score = np.array([[2,3,4],
                           [4,5,6],
-                          [7,8,9]))
-        return score[self.age]
+                          [7,8,9]])
+        return score[self._age]
 
 your = Student("mark")
 print(your.instructor)
+print()
+print(your.get_score())
