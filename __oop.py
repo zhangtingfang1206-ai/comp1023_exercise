ab556725f00b1bfefd18a0a77d57bb5d695ff6e2
你的名字 <>
2026-05-12 11:32:15 +0800

    update __oop.py

--- __oop.py
+++ __oop.py
@@ -1,16 +1,19 @@
+import numpy as np
+
 class Student:
-    def __init__(self):
+    def __init__(self, desmond):
         self.__name = 'g'  # two _ _ only allow internal methods can access
                            # new name: _Student__name
-        self._age = 34
-    def print(self):
-        print(self.__name) 
+        self._age = 4
+        self.instructor = desmond
 
-desmond = Student()
-desmond.print()
-print(desmond)
-desmond.__name = 'sdfg'
-desmond.id = 313
-print(desmond.__name)
-print(desmond.id)
-print(desmond)
+    def print(self):
+        print(self.__name)
+    def get_score(self):
+        score = np.array(([2,3,4],
+                          [4,5,6],
+                          [7,8,9]))
+        return score[self.age]
+
+your = Student("mark")
+print(your.instructor)
